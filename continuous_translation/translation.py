babdc17b5977deb68d49a55be63a13033ae8c8a3
Christian <christian@qualitytrade.com>
2023-10-27 07:52:55 +0800

    Specify again

--- continuous_translation/translation.py
+++ continuous_translation/translation.py
@@ -6,6 +6,7 @@ MARKDOWN_PROMPT = """
 Translate this markdown file while maintaining original formatting,
 including inline elements like links and images. Make sure to ignore text inside HTML tags, and code blocks like (```).
 Make sure to also ignore texts inside tabs or navigation tabs like ({% tabs %}), and gitbook's swagger blocks like ({% swagger %}, {% swagger-description %}, or {% swagger-response %})
+Make sure to ignore texts inside brackets like ([content])
 Be cautious when translating Markdown links,
 Markdown images, and Markdown headings. Make sure TOC links like (#content) are translated.
 """
